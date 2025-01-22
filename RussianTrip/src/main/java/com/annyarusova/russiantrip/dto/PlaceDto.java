64cd06cc8158872900a561f33ff48985e895a0c7
Fer0ty <artemiy.soloviev@gmail.com>
2025-01-22 16:38:50 +0300

    update: add id to response entity

--- RussianTrip/src/main/java/com/annyarusova/russiantrip/dto/PlaceDto.java
+++ RussianTrip/src/main/java/com/annyarusova/russiantrip/dto/PlaceDto.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlaceDto {
+    private Long id;
     private String name;
     private String description;
     private String location;
